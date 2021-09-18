691d1434c72412cf80f312c6e2d29af09c7069ac
Jonathan Villanueva <villanuevajonathan606@gmail.com>
2021-09-18 14:34:47 -0700

    commit tree d3754c4a59a8edad87be9aaef6e518dadc9e8e1c

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
 # Your code goes here!
+class Anagram 
+	def initialize(word)
+		@word = word
+	end 
+
+	def match(array)
+		array.select {|x| x.split("").sort == @word.split("").sort}
+	end 
+end 
